467d246cec01cab67627a379d908236972eca178
Abdul Rehman <132285684+AbdulRehman2008@users.noreply.github.com>
2026-06-23 09:41:58 +0500

    Enhance sentiment display and code formatting
    Updated sentiment display with emojis and improved code formatting.

--- app.py
+++ app.py
@@ -1,29 +1,23 @@
-# Step 3: Streamlit frontend
-
 import streamlit as st
 import requests
 
 st.title("🎬 IMDB Movie Review Sentiment")
 st.write("Type a movie review and the model will predict if it is Positive or Negative.")
 
-# Text input
 review = st.text_area("Enter your movie review here:")
 
-# Button
 if st.button("Predict"):
     if review == "":
         st.warning("Please write a review first.")
     else:
-        # Send request to FastAPI
-      response = requests.post("https://movie-model-backend-production.up.railway.app/predict", json={"text": review})
+        response = requests.post("https://movie-model-backend-production.up.railway.app/predict", json={"text": review})
         result = response.json()
-
         sentiment = result["sentiment"]
         score = result["score"]
 
         if sentiment == "Positive":
-            st.success("Sentiment: Positive")
+            st.success("Sentiment: Positive 😊")
         else:
-            st.error("Sentiment: Negative")
+            st.error("Sentiment: Negative 😞")
 
         st.write("Model Score:", score)
